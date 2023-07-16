c861b16dc3e0f000deacf77b4ab8b7d2f5d83e55
Kaiju234 <68970395+Kaiju234@users.noreply.github.com>
2023-07-16 16:53:51 -0500

    Add files via upload

---
+++ Projects/Web Replacement.py
@@ -0,0 +1,9 @@
+def replace_word():
+	
+	
+	str = "Hey Everyone im Raj and i have an ungodly combo of being a comnr"
+	word_to_replace = input("Enter the word to replace: ")
+	word_replacement = input("Enter the word replacement: ")
+	print(str.replace(word_to_replace,word_replacement))
+	
+replace_word()
